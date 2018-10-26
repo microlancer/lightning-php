58adabb5ce773ae53aeec0f15820956192d35812
Tim Horie <tak@thorie.com>
2018-10-25 18:33:34 -0700

    Added Lightning class

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # lightning-php
 PHP interface for the elements/c-lightning RPC API
+
+## Installation
+
+`composer require thorie7912/lightning-php:^1.0`
+
+## Usage
+
+```php
+$lightningApi = new \Lightning\LightningApi();
+$lightningApi->setRpcFile("/home/user/.lightning/lightning-rpc");
+
+// Get info
+
+$ret = $lightningApi->getInfo();
+
+// Pay invoice
+
+$invoice = "ln123xyz...";
+$ret = $lightningApi->pay($invoice);
+
+// Create invoice
+
+$msatoshi = 50000;
+$label = "Some label";
+$description = "Some description";
+$ret = $lightningApi->createinvoice($msatoshi, $label, $description);
+```
+
+
---
+++ src/Lightning.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Lightning;
+
+class LightningApi 
+{
+    private $rpcFile;
+
+    public function setRpcFile($rpcFile) 
+    {
+        $this->rpcFile = $rpcFile;
+    }
+
+    private function sendCommand(array $input) 
+    {
+        if (is_null($this->rpcFile)) {
+            throw new \Exception('rpcFile not set');
+        }
+        
+        $factory = new \Socket\Raw\Factory();
+        $socket = $factory->createClient($this->rpcFile);
+        $socket->setBlocking(true);
+        $socket->write(json_encode($input));
+        $data = "";
+        do {
+            $data .= $socket->read(8096);
+        } while ($socket->assertAlive() && $socket->selectRead(1.0));
+        $socket->close();
+        return json_decode($data);
+    }
+
+    public function getinfo() 
+    {
+        $input = [
+            "method" => "getinfo",
+            "params" => [],
+            "id" => 0
+        ];
+        return self::sendCommand($input);
+    }
+
+    public function invoice($msatoshi, $label, $description) 
+    {
+        $input = [
+            "method" => "invoice",
+            "params" => [
+                "msatoshi" => $msatoshi,
+                "label" => $label,
+                "description" => $description,
+            ],
+            "id" => 0
+        ];
+        return self::sendCommand($input);
+    }
+
+    public function listinvoices($label) 
+    {
+        $input = [
+            "method" => "listinvoices",
+            "params" => [
+                "label" => $label,
+            ],
+            "id" => 0
+        ];
+        return self::sendCommand($input);
+    }
+
+    public function decodepay($bolt11) 
+    {
+        $input = [
+            "method" => "decodepay",
+            "params" => [
+                "bolt11" => $bolt11,
+            ],
+            "id" => 0
+        ];
+        return self::sendCommand($input);
+    }
+
+    public function pay($bolt11, $msatoshi) 
+    {
+        $input = [
+            "method" => "pay",
+            "params" => [
+                "bolt11" => $bolt11,
+            ],
+            "id" => 0
+        ];
+        return self::sendCommand($input);
+    }
+
+}
